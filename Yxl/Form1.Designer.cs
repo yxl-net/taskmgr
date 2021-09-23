f9e68d1de43cb562a8e5d6dfd4a4287eae42d761
yxl-net <996986842@qq.com>
2021-09-23 20:20:35 +0800

    项目版本初始化

---
+++ Yxl/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Yxl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.ts = new System.Windows.Forms.ToolStrip();
+            this.tslFilter = new System.Windows.Forms.ToolStripLabel();
+            this.tstFilter = new System.Windows.Forms.ToolStripTextBox();
+            this.tsbRefresh = new System.Windows.Forms.ToolStripButton();
+            this.tss1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tscServices = new System.Windows.Forms.ToolStripButton();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.ts.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ts
+            // 
+            this.ts.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.ts.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslFilter,
+            this.tstFilter,
+            this.tsbRefresh,
+            this.tss1,
+            this.tscServices});
+            this.ts.Location = new System.Drawing.Point(0, 0);
+            this.ts.Name = "ts";
+            this.ts.Size = new System.Drawing.Size(800, 27);
+            this.ts.TabIndex = 0;
+            this.ts.Text = "toolStrip1";
+            // 
+            // tslFilter
+            // 
+            this.tslFilter.Name = "tslFilter";
+            this.tslFilter.Size = new System.Drawing.Size(54, 24);
+            this.tslFilter.Text = "搜索：";
+            // 
+            // tstFilter
+            // 
+            this.tstFilter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tstFilter.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.tstFilter.Name = "tstFilter";
+            this.tstFilter.Size = new System.Drawing.Size(100, 27);
+            // 
+            // tsbRefresh
+            // 
+            this.tsbRefresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbRefresh.Image = ((System.Drawing.Image)(resources.GetObject("tsbRefresh.Image")));
+            this.tsbRefresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbRefresh.Name = "tsbRefresh";
+            this.tsbRefresh.Size = new System.Drawing.Size(29, 24);
+            this.tsbRefresh.Text = "toolStripButton1";
+            // 
+            // tss1
+            // 
+            this.tss1.Name = "tss1";
+            this.tss1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // tscServices
+            // 
+            this.tscServices.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tscServices.Image = ((System.Drawing.Image)(resources.GetObject("tscServices.Image")));
+            this.tscServices.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tscServices.Name = "tscServices";
+            this.tscServices.RightToLeftAutoMirrorImage = true;
+            this.tscServices.Size = new System.Drawing.Size(29, 24);
+            this.tscServices.Text = "服务";
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.dgv.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv.Location = new System.Drawing.Point(0, 27);
+            this.dgv.MultiSelect = false;
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 27;
+            this.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgv.Size = new System.Drawing.Size(800, 423);
+            this.dgv.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.ts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ts.ResumeLayout(false);
+            this.ts.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip ts;
+        private System.Windows.Forms.ToolStripLabel tslFilter;
+        private System.Windows.Forms.ToolStripTextBox tstFilter;
+        private System.Windows.Forms.ToolStripButton tsbRefresh;
+        private System.Windows.Forms.ToolStripSeparator tss1;
+        private System.Windows.Forms.ToolStripButton tscServices;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
+
---
+++ Yxl/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yxl
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt=new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            dt.Clear();
+            dt.Columns.Add("名称");
+            dt.Columns.Add("描述");
+            dt.Columns.Add("启动类型");
+            dt.Columns.Add("状态");
+            dt.Columns.Add("可执行文件路径");
+            dt.Columns.Add("登录为");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Service");
+            string StartMode(object o)
+            {
+                switch (Convert.ToString(o))
+                {
+                    case "Manual":
+                        return "手动";
+                    case "Auto":
+                        return "自动";
+                    case "Disabled":
+                        return "禁用";
+                    default:
+                        return "未知";
+                }
+            }
+            string Started(object o)
+            {
+                return Convert.ToBoolean(o) ? "正在运行" : "已停止";
+            }
+            string StartName(object o)
+            {
+                switch (Convert.ToString(o))
+                {
+                    case "LocalSystem":
+                        return "本地系统";
+                    case "NT AUTHORITY\\LocalService":
+                        return "本地服务";
+                    case "NT Authority\\NetworkService":
+                        return "网络服务";
+                    default:
+                        return "未知";
+                }
+            }
+            foreach (ManagementObject mo in searcher.Get())
+            {
+                dt.Rows.Add( mo["Name"],mo["DisplayName"], StartMode(mo["StartMode"].ToString()), Started(mo["Started"]),mo["PathName"], StartName(mo["StartName"]));
+            }
+            //打开应用程序文件夹
+            dgv.DataSource = dt;
+            dgv.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+    }
+}
